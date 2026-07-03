3410ec2617859fe799f33d7357773ca9cd42c7da
168010 <vicky.kanyama@strathmore.edu>
2026-07-03 09:54:43 +0300

    Switched from SQLite to MySQL database

--- community_sports/settings.py
+++ community_sports/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'community_sports.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CommunitySports',
+        'USER': 'root',
+        'PASSWORD': 'Vicky123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
